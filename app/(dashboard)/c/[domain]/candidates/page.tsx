32778a49fd5c82d88502470a3d7f97316ae585c1
👻Abhishek Piedy <abhishek.piedy@gmail.com>
2025-12-08 14:29:11 -0500

    add basic page structure for candidates page

--- app/(dashboard)/c/[domain]/candidates/page.tsx
+++ app/(dashboard)/c/[domain]/candidates/page.tsx
@@ -1,3 +1,18 @@
-const CandidatesPage = () => <div>Candidates Page</div>;
+import { Search } from '@/app/components/Search';
+import { Button } from '@/components/ui/button';
+
+const CandidatesPage = () => (
+  <main>
+    <h1 className='text-2xl font-semibold'>Candidate management</h1>
+
+    <div className='mt-5 flex items-center justify-between gap-2'>
+      <Search />
+
+      <Button>Add candidate</Button>
+    </div>
+
+    <div className='mt-6 w-full'>Candidates data table</div>
+  </main>
+);
 
 export default CandidatesPage;
