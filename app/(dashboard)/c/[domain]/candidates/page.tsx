b2ec83d071c746f84e57bb44462364484d819e93
👻Abhishek Piedy <abhishek.piedy@gmail.com>
2025-12-09 18:07:41 -0500

    hydrate candidates table and org candidates components

--- app/(dashboard)/c/[domain]/candidates/page.tsx
+++ app/(dashboard)/c/[domain]/candidates/page.tsx
@@ -1,18 +1,34 @@
+import { CandidatesTableColumns } from '@/app/(dashboard)/components/candidates/CandidatesTableColumns';
+import { DataTable } from '@/app/(dashboard)/components/candidates/DataTable';
 import { Search } from '@/app/components/Search';
-import { Button } from '@/components/ui/button';
+import { PlusIcon } from '@heroicons/react/24/outline';
+import { getCandidates } from '@/lib/data/candidates/candidateData';
+import Link from 'next/link';
 
-const CandidatesPage = () => (
-  <main>
-    <h1 className='text-2xl font-semibold'>Candidate management</h1>
+const CandidatesPage = async () => {
+  const candidates = await getCandidates();
 
-    <div className='mt-5 flex items-center justify-between gap-2'>
-      <Search />
+  return (
+    <main>
+      <h1 className='text-2xl font-semibold'>Candidate management</h1>
 
-      <Button>Add candidate</Button>
-    </div>
+      <div className='mt-5 flex items-center justify-between gap-2'>
+        <Search />
 
-    <div className='mt-6 w-full'>Candidates data table</div>
-  </main>
-);
+        <Link
+          href='candidates/new'
+          className='bg-primary text-primary-foreground hover:bg-primary/90 inline-flex items-center justify-center gap-2 rounded-md px-4 py-2 text-sm font-medium'
+        >
+          <PlusIcon className='size-4' />
+          <span>Add candidate</span>
+        </Link>
+      </div>
+
+      <div className='mt-6 w-full'>
+        <DataTable data={candidates} columns={CandidatesTableColumns} />
+      </div>
+    </main>
+  );
+};
 
 export default CandidatesPage;
---
+++ app/(dashboard)/components/candidates/CandidatesTableColumns.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { ColumnDef } from '@tanstack/react-table';
+import { Candidate } from '@/lib/data/candidates/candidateTypes';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+
+export const CandidatesTableColumns: ColumnDef<Candidate>[] = [
+  {
+    id: 'candidateName',
+    header: 'Name',
+    cell: ({ row }) => {
+      const { firstName, lastName, city, state, country, profileImageUrl } = row.original;
+
+      return (
+        <div className='flex items-center gap-1'>
+          <div>
+            <Avatar className='size-10'>
+              <AvatarImage src={profileImageUrl} alt='' />
+              <AvatarFallback className='bg-black text-white'>
+                {firstName.at(0)}
+                {lastName.at(0)}
+              </AvatarFallback>
+            </Avatar>
+          </div>
+
+          <div className='flex flex-col'>
+            <span className='font-medium'>
+              {firstName} {lastName}
+            </span>
+
+            <span className='text-sm text-gray-500'>
+              {city ? `${city}` : ''}
+              {state ? `, ${state}` : ''}
+              {country && country !== 'USA' ? `, ${country}` : ''}
+            </span>
+          </div>
+        </div>
+      );
+    },
+  },
+  { accessorKey: 'title', header: 'Title' },
+  { accessorKey: 'status', header: 'Status' },
+  { accessorKey: 'availability', header: 'Availability' },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const { id } = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='h-8 w-8 cursor-pointer p-0'>
+              <span className='sr-only'>Open menu</span>
+              <EllipsisHorizontalIcon className='h-4 w-4' />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem>
+              <Link href={`candidates/${id}`}>View details</Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
