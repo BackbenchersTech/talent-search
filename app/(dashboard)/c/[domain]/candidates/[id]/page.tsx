04ffd1a9023f1df8037e8d24be244154ad709480
👻Abhishek Piedy <abhishek.piedy@gmail.com>
2025-12-11 20:53:32 -0500

    change fetch order and clean up candidate detail page

--- app/(dashboard)/c/[domain]/candidates/[id]/page.tsx
+++ app/(dashboard)/c/[domain]/candidates/[id]/page.tsx
@@ -22,20 +22,21 @@ const CandidateDetailPage = async (props: { params: Promise<{ id: string }> }) =
   const decodedCandidateId = decodeUUID(encodedCandidateId);
   const candidate = await getCandidateById(decodedCandidateId);
   const jobProfiles = await getCandidateProfiles(decodedCandidateId);
-  const profilesWithCandidate = jobProfiles.map((jp) => ({
-    ...jp,
-    candidate: {
-      id: candidate!.id,
-      city: candidate!.city,
-      state: candidate!.state,
-      country: candidate!.country,
-      availability: candidate!.availability,
-    },
-  }));
 
   if (!candidate) {
     notFound();
   }
+
+  const profilesWithCandidate = jobProfiles.map((jp) => ({
+    ...jp,
+    candidate: {
+      id: candidate.id,
+      city: candidate.city,
+      state: candidate.state,
+      country: candidate.country,
+      availability: candidate.availability,
+    },
+  }));
 
   const { firstName, lastName, city, state, country, profileImageUrl } = candidate;
 
@@ -49,7 +50,8 @@ const CandidateDetailPage = async (props: { params: Promise<{ id: string }> }) =
         <span className='font-medium'>View all candidates</span>
       </Link>
 
-      {/* TODO: add tags in header */}
+      {/* TODO: add status tag and linkedin link as icon in header */}
+      {/* TODO: actions => make active/inactive, edit */}
       <div className='flex items-center gap-4'>
         <>
           <Avatar className='size-13'>
@@ -79,15 +81,7 @@ const CandidateDetailPage = async (props: { params: Promise<{ id: string }> }) =
       </div>
 
       <section>
-        {/* all of this is internal view only */}
-        <h2 className='mt-6 mb-3 text-lg font-medium'>Job profiles</h2>
-        <ProfileGrid>
-          {/* fetch profiles for candidate */}
-          {profilesWithCandidate.map((p) => (
-            <ProfileCard key={p.id} profileWithCandidate={p} />
-          ))}
-        </ProfileGrid>
-
+        {/* TODO: implement a single row/card that shows all of this */}
         {/* TODO: split the cards into individual client components that have the edit buttons functionality built into them */}
         <Card className='mt-6 shadow-none'>
           <CardHeader>
@@ -96,51 +90,37 @@ const CandidateDetailPage = async (props: { params: Promise<{ id: string }> }) =
           </CardHeader>
 
           <CardContent>
-            <p>
-              name, title, location, availability, salary expectations, total years of
-              experience
-            </p>
+            <p>email &middot; • phone, salary expected range</p>
           </CardContent>
         </Card>
 
-        <Card className='mt-6 shadow-none'>
-          <CardHeader>
-            <CardTitle>Contact</CardTitle>
-            <CardAction>edit button on hover</CardAction>
-          </CardHeader>
+        <h2 className='mt-6 mb-3 text-lg font-medium'>Job profiles</h2>
+        <ProfileGrid>
+          {/* fetch profiles for candidate */}
+          {profilesWithCandidate.map((p) => (
+            <ProfileCard key={p.id} profileWithCandidate={p} />
+          ))}
+        </ProfileGrid>
 
-          <CardContent>
-            <p>email, phone, linkedin</p>
-          </CardContent>
-        </Card>
+        <span className='inline-flex items-center gap-2 rounded-full border border-gray-200 bg-white px-3 py-1 shadow-sm'>
+          <span className='flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-br from-emerald-400 via-emerald-500 to-emerald-600 shadow-sm ring-2 ring-white' />
 
-        <Card className='mt-6 shadow-none'>
-          <CardHeader>
-            <CardTitle>Skills & Tags</CardTitle>
-            <CardAction>edit button on hover</CardAction>
-          </CardHeader>
+          <span className='text-sm font-medium text-gray-800'>Active</span>
+        </span>
 
-          <CardContent>
-            <p>Skill chips. should this be in profiles?</p>
-          </CardContent>
-        </Card>
+        <span className='inline-flex items-center gap-2 rounded-full border border-gray-200 bg-white px-3 py-1 shadow-sm'>
+          <span className='flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-br from-red-300 via-red-500 to-red-600 shadow-sm ring-2 ring-white' />
 
-        <Card className='mt-6 shadow-none'>
-          <CardHeader>
-            <CardTitle>Resumes</CardTitle>
-            <CardAction>edit button on hover</CardAction>
-          </CardHeader>
+          <span className='text-sm font-medium text-gray-800'>Inactive</span>
+        </span>
 
-          <CardContent>
-            <p>PDF preview, parsed summary</p>
-          </CardContent>
-        </Card>
+        <span className='inline-flex items-center gap-2 rounded-full border border-gray-200 bg-white px-3 py-1 shadow-sm'>
+          <span className='flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-br from-gray-300 via-gray-400 to-gray-500 shadow-sm ring-2 ring-white' />
 
-        <span>public profiles section</span>
-        <br />
+          <span className='text-sm font-medium text-gray-700'>Draft</span>
+        </span>
 
-        <span>hoverable notes section that internal team adds</span>
-        <br />
+        <div>hoverable notes section that internal team adds</div>
       </section>
     </main>
   );
