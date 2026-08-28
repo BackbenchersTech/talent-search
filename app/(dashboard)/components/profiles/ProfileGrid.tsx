50b8d302cfe57cc10671c2165f060a33f8574b2c
👻Abhishek Piedy <abhishek.piedy@gmail.com>
2026-08-28 17:02:06 -0400

    increase card width in profile grid

--- app/(dashboard)/components/profiles/ProfileGrid.tsx
+++ app/(dashboard)/components/profiles/ProfileGrid.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 
 export const ProfileGrid = ({ children }: { children: ReactNode }) => (
-  <div className='grid w-full grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-5'>
+  <div className='grid w-full grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-5'>
     {children}
   </div>
 );
